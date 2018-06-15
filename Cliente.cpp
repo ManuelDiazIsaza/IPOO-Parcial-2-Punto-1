89fec7764d5879a10595ab6c29abcde0e6428e6e
Manuel Diaz <manuel.isaza@correounivalle.edu.co>
2018-06-14 22:57:17 -0500

    Completo

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,12 +1,15 @@
 /*
-  Archivo: Nodo.cpp
-  Autor: Luis Yovany Romo Portilla
-  Email: luis.romo@correounivalle.edu.co
-  Fecha creaciC3n: 2017-08-31
-  Fecha C:ltima modificaciC3n: 2017-08-31
-  VersiC3n: 0.1
+  Archivo: cliente.cpp
+  Autor: Manuel Diaz COD 1741652 - Jeffrey Rios COD 1744831
+  Email: manuel.isaza@correounivalle.edu.co - jeffrey.rios@correounivalle.edu.co
+  Fecha creacion: 2018-06-14
+  Fecha ultima modificacion: 2018-06-14
+  Version: 0.1
   Licencia: GPL
 */
+
+// Responsabilidad:
+// Colaboracion: ninguna
 
 #include <stdio.h>
 #include <iostream>
